92d3ad21610de83c70a3a5aa472153a1d3d798df
Chatnarint Boonsaeng <zenlektomyum@gmail.com>
2025-02-20 13:16:46 +0700

    feat: project

---
+++ Project/main.py
@@ -0,0 +1,32 @@
+class User:
+    pass
+
+class Flight:
+    pass
+
+class Booking:
+    pass
+
+class Service:
+    pass
+
+class Passenger:
+    pass
+
+class Airlinewa:
+    def api_book(user_id, flight_instance_id, list_servce:list):
+        pass
+
+    def get_user(user_id) -> User:
+        pass
+
+    def get_flight_instance(flight_instance_id) -> Flight:
+        pass
+
+    def create_booking(user_instance:User, flight_instance:Flight):
+        pass
+
+    def calculate_payment(booking_instance:Booking):
+        pass
+
+
